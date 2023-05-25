28c81c46d80d67605ba3408963a914a5cd046aa2
MisaoDev <69985862+MisaoDev@users.noreply.github.com>
2023-05-25 17:00:41 -0400

    fix static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   ssr: false,
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
+    baseURL: '/form-test/'
   },
 
   css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css'],
