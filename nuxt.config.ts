6f1dd31b8601566e9307ea35672a17826cdc4928
MisaoDev <69985862+MisaoDev@users.noreply.github.com>
2023-05-25 17:23:16 -0400

    remove static site

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@pinia/nuxt'],
-  ssr: false,
+  // ssr: false,
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
-    baseURL: '/form-test/'
   },
 
   css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css'],
