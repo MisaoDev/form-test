d20456df676fa98ce7980173d1da3ed1dd0710b0
MisaoDev <69985862+MisaoDev@users.noreply.github.com>
2023-05-25 16:49:22 -0400

    generate static

--- pages/index.js
+++
@@ -1,3 +0,0 @@
-definePageMeta({
-  redirect: '/mainpage',
-})
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+div
+</template>
+
+<script setup>
+useAsyncData(async () => { await navigateTo('/mainpage') })
+
+// definePageMeta({
+//   redirect: '/mainpage',
+// })
+</script>
+
+<style lang="scss" scoped>
+
+</style>
